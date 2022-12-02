9de75f13867b307408b23f737c522ec6e2ea913a
Adrikikicp <adrikikicp@gmail.com>
2022-12-02 18:40:51 -0500

    fix errors of duplication

--- fabric/src/main/java/com/awkard_lib/awkard/AwkardF.java
+++ fabric/src/main/java/com/awkard_lib/awkard/AwkardF.java
@@ -3,7 +3,7 @@ package com.awkard_lib.awkard;
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 
-public class Awkard implements ModInitializer {
+public class AwkardF implements ModInitializer {
     @Override
     public void onInitialize() {
         LogUtils.getLogger().info("Awkard lib");
